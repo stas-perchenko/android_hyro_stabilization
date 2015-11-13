8c765d8eb3c9d742851706dacb42b702afe3ff28
Stanislav Perchenko <sperchenko@gmail.com>
2015-11-13 16:16:54 +0200

    Skeleton of my own vertical SeekView

---
+++ app/src/main/java/com/alperez/hyrocam/VerticalSeekBar.java
@@ -0,0 +1,78 @@
+package com.alperez.hyrocam;
+
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by stanislav.perchenko on 13-Nov-15.
+ */
+public class VerticalSeekBar extends View {
+
+
+
+    // Sizes of content items
+    private int
+
+
+
+    // Cached values of paddings
+    private int paddingLeft;
+    private int paddingTop;
+    private int paddingRight;
+    private int paddingBottom;
+
+    // Size and start position of content area regarding to View's size and paddings.
+    private int contentAreaStartX;
+    private int contentAreaStartY;
+    private int contentAreaWidth;
+    private int contentAreaHeight;
+
+
+
+
+    private void init() {
+
+        //TODO
+        updatePaddingInternal();
+    }
+
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom) {
+        super.setPadding(left, top, right, bottom);
+        updatePaddingInternal();
+        updateContentAreaSize(getWidth(), getHeight());
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        updateContentAreaSize(w, h);
+    }
+
+    private void updatePaddingInternal() {
+        paddingLeft = getPaddingLeft();
+        paddingTop = getPaddingTop();
+        paddingRight = getPaddingRight();
+        paddingBottom = getPaddingBottom();
+    }
+
+    private void updateContentAreaSize(int viewWidth, int viewHeight) {
+        contentAreaStartX = paddingLeft;
+        contentAreaStartY = paddingTop;
+        contentAreaWidth = viewWidth - paddingLeft - paddingRight;
+        if (contentAreaWidth < 0) contentAreaWidth = 0;
+        contentAreaHeight = viewHeight - paddingTop - paddingBottom;
+        if (contentAreaHeight < 0) contentAreaHeight = 0;
+
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (contentAreaWidth == 0 || contentAreaHeight == 0) return;
+        canvas.save();
+        canvas.clipRect(contentAreaStartX, contentAreaStartY, contentAreaStartX+contentAreaWidth, contentAreaStartY+contentAreaHeight);
+        //TODO Draw here
+        canvas.restore();
+    }
+}
